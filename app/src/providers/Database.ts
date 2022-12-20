b365e349a6ba273cc44e092fdb21384e12af0386
Lioncat2002 <sankhayan2002@gmail.com>
2022-12-20 21:00:08 +0530

    refactor: remove debug lines

--- app/src/providers/Database.ts
+++ app/src/providers/Database.ts
@@ -4,7 +4,6 @@ import Log from "../middlewares/Log";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log(process.env.DB_USER)
 
 export class Database {
   public static async init(): Promise<void> {
