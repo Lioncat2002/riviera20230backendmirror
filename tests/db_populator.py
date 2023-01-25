a76635fabc5c263c3c47cf9b88c782966df20108
0x5b-5245444143544544-5d <vignesh.natarajan2021@vitstudent.ac.in>
2023-01-25 15:50:26 +0530

    allow seats to be empty

--- tests/db_populator.py
+++ tests/db_populator.py
@@ -4,12 +4,18 @@ import random
 xls = pd.ExcelFile("events-sports2.xlsx")
 df1 = pd.read_excel(xls, "Individual Events")
 df2 = pd.read_excel(xls, "All Events Description")
+df3 = pd.read_excel(xls, "Team Events")
 df1 = df1[["Event Name", "Club/ Chapter/ Individual",
            "Category", "Total Participant Limit", "Registration Fees", "Base Fees", "Fees SGST(%9)", "Fees CGST(%9)"]].copy()
 df2 = df2[["Event Name", "Event Description", "Date & Time", "Venue"]].copy()
+df3 = df3[["Event Name", "Club/ Chapter/ Individual",
+           "Category", "Maximum Limit (teams)", "Max Members/Team", "Total Event Cost", "Base Cost", "SGST(%9)", "CGST(%9)"]].copy()
 df1 = df1.merge(df2, how='left', on="Event Name")
 
+df3 = df3.merge(df2, how='left', on="Event Name")
+
 df1 = df1.reset_index()
+df3 = df3.reset_index()
 
 for index, row in df1.iterrows():
    category = row["Category"]
@@ -40,6 +46,46 @@ for index, row in df1.iterrows():
        "total_cost": total_cost,
        "seats": seats,
        "is_team_event": "false",
+       "featured": random.choice(["true", "false"])
+   }
+   #r = requests.put('http://localhost:3000/events/bruh', data=json)
+   #print(r.json())
+   #input("type something to continue")
+
+for index, row in df3.iterrows():
+   category = str(row["Category"])
+   name = str(row["Event Name"])
+   organizing_body = str(row["Club/ Chapter/ Individual"])
+   description = str(row["Event Description"])
+   total_cost = int(row["Total Event Cost"])
+   date = str(row['Date & Time'])
+   start = f"{date[6:10]}-{date[0:2]}-{date[3:5]}T{date[11:]}"
+   # 02-23-2023T12:00:00.000Z
+   # 02-12-2023T10:30:00.000Z
+   end = start[:11] + "14:40:00.000Z"
+   location = str(row['Venue'])
+   base_cost = int(row["Base Cost"])
+   sgst = row["SGST(%9)"]
+   cgst = row["CGST(%9)"]
+   team_max_members = int(row["Max Members/Team"])
+   teams = row["Maximum Limit (teams)"]
+   seats = ""
+
+   json = {
+       "name": name,
+       "organizing_body": organizing_body,
+       "image_url": "https://picsum.photos/200",
+       "start": start,
+       "end": end,
+       "loc": location,
+       "description": description,
+       "instructions": random.choice(["hello there", "towards left", "right", "upside", "down"]),
+       "event_type": category,
+       "total_cost": total_cost,
+       "teams": teams,
+       "seats": seats,
+       "team_max_members": team_max_members,
+       "is_team_event": "true",
        "featured": random.choice(["true", "false"])
    }
    r = requests.put('http://localhost:3000/events/test', data=json)
