4d84a796032b859a79aa6155d221827f2d3e6736
Lioncat2002 <sankhayan2002@gmail.com>
2023-01-25 16:07:37 +0530

    stuff

--- tests/test.py
+++ tests/test.py
@@ -1,15 +1,25 @@
 import requests
 import csv
-
+import os
+def upload_image_to_imgur(image_path):
+    CLIENT_ID = "546c25a59c58ad7"
+    HEADERS = {'Authorization': 'Client-ID ' + CLIENT_ID}
+    URL = "https://api.imgur.com/3/image"
+    with open(image_path, 'rb') as f:
+        r = requests.post(URL, files={'image': f}, headers=HEADERS)
+        return r.json()['data']['link']
 
 def test():
 
-    json = {
-        "name": "Skills N Thrills",
-        "image_url": "https://i.imgur.com/tfwhkf1.jpg",
-    }
-    r = requests.patch('https://riviera.fly.dev/events/bruh', data=json)
-    print(r.text)
+    for path in os.listdir("images"):
+        lnk=upload_image_to_imgur(f"images/{path}")
+
+        json = {
+            "name": path.split(".")[0],
+            "image_url": lnk,
+         }
+        r = requests.patch('https://riviera.fly.dev/events/bruh', data=json)
+        print(r.text)
 
 
 test()
