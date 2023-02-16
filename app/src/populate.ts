937de62a1a68ce7e0821eb776c360424cf438a21
Anuj Parihar <anujparihar@yahoo.com>
2023-02-16 19:22:37 +0530

    fix: population script (#38)

--- app/src/populate.ts
+++ app/src/populate.ts
@@ -2,15 +2,24 @@ import eventsData from "./data.json";
 import Events from "./models/event_v2.model";
 import App from "./providers/App";
 
-
 App.loadDatabase();
 
-for (const eventData of eventsData) {
-  const event = new Events(eventData);
-  event.save((err: any) => {
-    if (err) {
-      console.log(err);
+// Drop the Events collection
+Events.collection.drop((err: any) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Events collection dropped");
+
+    // Populate the Events collection
+    for (const eventData of eventsData) {
+      const event = new Events(eventData);
+      event.save((err: any) => {
+        if (err) {
+          console.log(err);
+        }
+      });
     }
-  });
-}
-console.log("Events populated");
+    console.log("Events populated");
+  }
+});
